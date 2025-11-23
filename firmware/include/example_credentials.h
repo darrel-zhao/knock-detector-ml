b3db3c8a22bae36af7fccdc0eedba5e26da1a39f
Darrel Zhao <darrelzhao06@gmail.com>
2025-11-23 11:37:06 -0600

    Remove credentials.h from committed files

---
+++ firmware/include/example_credentials.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Use typed string constants with unique names to avoid macro collisions
+static const char SSID[] = "REPLACE_ME_SSID";
+static const char PASSWORD[] = "REPLACE_ME_PASSWORD";
+
+static const char IP_ADDRESS[] = "REPLACE_ME_IP_ADDRESS"; 
